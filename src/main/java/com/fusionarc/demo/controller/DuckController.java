77c33293f5020edae98b1a820b3b18bbe297c806
David Barrow <davecheez21@gmail.com>
2022-03-08 16:20:11 +0000

    add crud feature

---
+++ src/main/java/com/fusionarc/demo/controller/DuckController.java
@@ -0,0 +1,81 @@
+package com.fusionarc.demo.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fusionarc.demo.entity.Duck;
+import com.fusionarc.demo.service.DuckService;
+
+@RestController
+@RequestMapping("/duck")
+public class DuckController {
+
+	private DuckService service;
+
+	private DuckController(DuckService service) {
+		this.service = service;
+	}
+
+	// Create
+	// @RequestBody allows us to pass through an object/data when we make the
+	// request
+
+	@PostMapping("/create")
+	public ResponseEntity<Duck> createDuck(@RequestBody Duck duck) {
+		return new ResponseEntity<Duck>(this.service.create(duck), HttpStatus.CREATED);
+	}
+
+	// Read All
+	@GetMapping("/readAll")
+	public ResponseEntity<List<Duck>> readAllDucks() {
+		return new ResponseEntity<List<Duck>>(this.service.readAll(), HttpStatus.OK);
+	}
+
+	// Read by Id
+	// @PathVariable allows us to pass a variable (in this case ID) to the path &
+	// the service.readById method
+	@GetMapping("/readById/{id}")
+	public ResponseEntity<Duck> readById(@PathVariable long id, @RequestBody Duck duck) {
+		return new ResponseEntity<Duck>(this.service.readById(id), HttpStatus.OK);
+	}
+
+	// Update
+	// update requires both RequestBody and PathVariable as it takes in the id, and
+	// also passes through the new object information
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Duck> updateDuck(@PathVariable long id, @RequestBody Duck duck) {
+		return new ResponseEntity<Duck>(this.service.update(id, duck), HttpStatus.ACCEPTED);
+	}
+
+	// Delete
+	// Ternary operator, IF service.delete goes through (deletes the id), return no
+	// content, ELSE return not found
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Boolean> deleteDuck(@PathVariable long id) {
+		return (this.service.delete(id)) ? new ResponseEntity<Boolean>(HttpStatus.NO_CONTENT) : 
+			new ResponseEntity<Boolean>(HttpStatus.NOT_FOUND);
+	}
+
+}
+	
+	/*  This is the same as above, but written using if/else statements
+	
+	if(this.service.delete(id) == true) {
+		new ResponseEntity<Boolean>(HttpStatus.NO_CONTENT)
+	} else {
+		new ResponseEntity<Boolean>(HttpStatus.NOT_FOUND)
+	}
+	
+	*/
+
